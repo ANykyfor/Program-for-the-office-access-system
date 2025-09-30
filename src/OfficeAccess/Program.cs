4e5b0882e78b49c2922a695058d57bead6b1580c
ANykyfor <artproger@outlook.com>
2025-09-30 21:38:38 +0300

    add case 1,2

--- src/OfficeAccess/Program.cs
+++ src/OfficeAccess/Program.cs
@@ -7,11 +7,41 @@ static class Program
 {
     static void Main()
     {
+        Console.InputEncoding  = System.Text.Encoding.UTF8;
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.WriteLine("Введіть рівень доступу (1-5):  ");
+
+        Console.WriteLine("Введіть рівень доступу (1-5): ");
         if (!int.TryParse(Console.ReadLine(), out int level) || level < 1 || level > 5)
         {
-            System.Console.WriteLine("Некоректний рівень доступу, у вас ще є декілька спроб , в інакшому випадку я дістаю Кольт.");
+            Console.WriteLine("Некоректний рівень доступу, маєте ще є декілька спроб , в інакшому випадку я дістаю Кольт.");
             return;
         }
-        System.Console.WriteLine("Введіть час доби (ранок/день/вечір/ніч):  ");
+        Console.WriteLine("Введіть час доби (ранок/день/вечір/ніч):  ");
+        var timeRaw = (Console.ReadLine() ?? "").Trim().ToLowerInvariant();
+         if (!TryParseDayPart(timeRaw, out var part))
+        {
+            Console.WriteLine("Помилка: некоректний час доби.");
+            return;
+        }
+        switch (level)
+        {
+            case 1:
+                Console.WriteLine(part == DayPart.Day ? "Доступ дозволено : зона - хол." : "Доступ заборонено.");
+                break;
+            case 2:
+                Console.WriteLine((part == DayPart.Morning || part == DayPart.Day) ? "Доступ дозволено: зони - хол, робочі зали." : "Доступ заборонено.");
+                break;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+   }
