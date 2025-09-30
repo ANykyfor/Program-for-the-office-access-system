48f0164f2781fe8142c1f14994320364b4a63668
ANykyfor <artproger@outlook.com>
2025-09-30 21:10:53 +0300

    Add base logical Access

--- src/OfficeAccess/Program.cs
+++ src/OfficeAccess/Program.cs
@@ -8,12 +8,10 @@ static class Program
     static void Main()
     {
         Console.OutputEncoding = System.Text.Encoding.UTF8;
-        //Робимо перший рівень доступу 
         Console.WriteLine("Введіть рівень доступу (1-5):  ");
         if (!int.TryParse(Console.ReadLine(), out int level) || level < 1 || level > 5)
         {
             System.Console.WriteLine("Некоректний рівень доступу, у вас ще є декілька спроб , в інакшому випадку я дістаю Кольт.");
             return;
         }
-        //Тепер накалякаем час доби 
         System.Console.WriteLine("Введіть час доби (ранок/день/вечір/ніч):  ");
